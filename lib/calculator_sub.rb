c72ea0826f4cdf5e83fbe22517f4c9b21213e4b9
vagrant <lavendersally062@yahoo.co.jp>
2015-08-01 06:24:47 +0000

    fix sub method

--- lib/calculator_sub.rb
+++ lib/calculator_sub.rb
@@ -1,3 +1,3 @@
 def calculator_sub(num1, num2)
-    return 0
+    return num1 - num2
 end
--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -11,6 +11,9 @@ class CalculatorTest < Minitest::Test
 
     def test_sub
         assert @calc.sub(0, 0) == 0, "sub(0, 0) failed"
+        assert @calc.sub(8, 2) == 6, "sub(8, 2) failed"
+        assert @calc.sub(20, 4) == 16, "sub(20, 4) failed"
+        assert @calc.sub(100, 2) == 98, "sub(100, 2) failed"
     end
 
     def test_mul
